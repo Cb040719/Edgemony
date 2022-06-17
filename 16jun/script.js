95e897e45265add31f3fa66cc735737effcbad79
Cb040719 <claudiob0162@gmail.com>
2022-06-17 09:30:48 +0200

    upload

---
+++ 16jun/script.js
@@ -0,0 +1,64 @@
+function sum(a, b){
+    return a + b;
+}
+
+function subtraction(a, b){
+    return a - b;
+}
+
+function multiplication(a, b){
+    return a * b; 
+}
+
+function division(a, b){
+    return a / b;
+}
+
+
+const nNumbers = prompt("Su quanti numeri vuoi eseguire l'operazione?");
+const parse_nNumbers = parseInt(nNumbers);
+
+let aNumber = [];
+
+
+for(i=0; i<nNumbers; i++){
+    const getPromptValue = prompt("Inserisci un numero.");
+    aNumber.push(parseInt(getPromptValue));
+    console.log(getPromptValue, aNumber);
+}
+
+
+    const Operation = prompt("Scegli l'operazione desiderata");
+    
+    let summa = aNumber[0];
+    let subba = aNumber[0];
+    let molty = aNumber[0];
+    let divvy = aNumber[0];
+    aNumber[0] = 0;
+
+    switch(Operation) {
+        case "+":
+            for(i=0; i<nNumbers; i++){ 
+                summa = sum(summa, aNumber[i]);
+                console.log(summa);
+            }
+            
+            break;
+        case "-":
+            for(i=0; i<nNumbers; i++){
+                
+            }
+            break;
+    
+        case "*":
+            console.log("Risultato: ", parseFirstNum * parseSecondNum)
+            break;
+        
+        case "/":
+            console.log("Risultato: ", parseFirstNum / parseSecondNum)
+            break;
+    
+        default:
+            alert("Non hai specificato l'operazione")
+    }
+    
