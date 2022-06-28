0312976d4b36ce65da172bcb154e9647249308f1
Cb040719 <claudiob0162@gmail.com>
2022-06-28 16:43:08 +0200

    fix

--- 27jun/script.js
+++ 27jun/script.js
@@ -1,31 +1,22 @@
 // (function () {
-    const newElement = ($element) => document.createElement($element);
-    const choice = prompt("Scegli un elemento da creare");
+const newElement = ($element) => document.createElement($element);
+const choice = prompt("Scegli un elemento da creare");
     
-    const createCard = () => {
-        const $cardContainer = newElement("div");
-        const $cardTitle = newElement("h2");
-        const $cardImage = document.querySelector(".imgUpload");
-        const $cardText = newElement("p");
+const createCard = () => {
+    const $cardContainer = newElement("div");
+    const $cardTitle = newElement("h2");
+    const $cardImageInput = document.querySelector(".imgUpload");
+    const $cardImage = newElement("img");
+    const $cardText = newElement("p");
         
-        document.body.append($cardContainer);
-        $cardContainer.append($cardTitle, $cardImage, $cardText);
+    document.body.append($cardContainer);
+    $cardContainer.append($cardTitle, $cardImage, $cardText);
         
-        $cardContainer.className = "cardContainer";
-        $cardTitle.className = "cardTitle";
-        $cardImage.className = "cardImage";
-        $cardText.className = "cardText";
-        
-        const cardImgAlt = prompt("Inserisci un testo sostitutivo per l'immagine");
-        $cardImage.setAttribute("type", image);
-            // $cardImage.setAttribute("alt", cardImgAlt);
-        
-        // const uploadCardImg = () => {
-        //     cardImgInput.
-            // cardImg = cardImgInput.setAttribute("type", Image);
-        //     $cardImage = cardImgInput;
-        // // }
-    }
+    $cardContainer.className = "cardContainer";
+    $cardTitle.className = "cardTitle";
+    $cardImage.className = "cardImage";
+    $cardText.className = "cardText";
+}
     
     switch(choice){
         case "card":
