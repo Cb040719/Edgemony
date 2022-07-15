87d7e90be2f7b797f88a1b5fb59525f64666d7c7
Cb040719 <claudiob0162@gmail.com>
2022-07-15 03:17:36 +0200

    everything up to date

--- jul07/script.js
+++ jul07/script.js
@@ -1,13 +1,47 @@
 (function() {
 
+    const $taskInput = document.querySelector("#newTaskInput");
+    const $taskList = document.querySelector("#taskList");
+    
     const getInputText = () => {
-        const $newTask = document.querySelector("#newTaskInput");
-        console.log($newTask.value);
-        $newTask.value = ""
+
+        const date = new Date();
+        const currentDate = `${date.getHours()} : ${date.getMinutes()}`;
+
+        const $newTask = `
+        <li>
+        <input type="checkbox"/>
+        <span>${$taskInput.value} - ${currentDate}</span>
+        <span>
+            <button id="deleteButton">Scancella</button>
+        </span>
+        </li>`;
+
+        
+
+        $taskList.innerHTML += $newTask;  
+
+        
+        console.log($taskInput.value + " - " + currentDate);
+        $taskInput.value = ""
     }
+    
+    $taskList.addEventListener("click", function (event) {
+        const deleteButton = event.target;
+
+        if(deleteButton.tagName === "BUTTON"){
+            const targetTask = deleteButton.closest("li")
+            
+            if(targetTask){
+                targetTask.remove();
+                console.clear();
+            }
+        }
+    })
 
     const $submitButton = document.querySelector("#addNewTask");
-
     $submitButton.addEventListener("click", getInputText);
+    
+
 
 })()
